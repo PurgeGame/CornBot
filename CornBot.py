d9dae60d1a6500f0c4781191c97d87d09fc48f9a
Purge Game <purgegamenft@gmail.com>
2024-03-24 07:26:02 -0500

    added ath and search

--- CornBot.py
+++ CornBot.py
@@ -7,18 +7,30 @@ import math
 import asyncio
 from prettytable import PrettyTable
 import aiohttp
+import random
+
 
 GECKO_API = os.environ.get("GECKO_API")
 
 intents = discord.Intents.default()  # Create a new Intents object with default settings
 bot = discord.Bot(intents=intents)  # Pass the Intents object to the Bot
 
+token = os.environ.get("DISCORD_BOT_SECRET")
+global change_btc
+change_btc = None
+
+
+
+
+
+
+
 async def get_prices(coins):
     # Convert the list of coins to a comma-separated string
     coins_str = ','.join(coins)
 
     # Define the URL and headers
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coins_str}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true&precision=full"
+    url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coins_str}"
     headers = {"x-cg-demo-api-key": GECKO_API}
 
     # Send a request to the CoinGecko API
@@ -28,8 +40,11 @@ async def get_prices(coins):
 
     # Check if the request was successful
     if response.status == 200:
+        # Convert the list to a dictionary
+        prices = {coin['id']: coin for coin in data}
+
         # Return the prices
-        return data
+        return prices
     else:
         # If the request was not successful, return an empty dictionary
         return {}
@@ -52,6 +67,14 @@ def check_coin(coin):
             return coin
     else:
         return False
+def format(price):
+    if price > 100:
+        price = f'{int(price):,}' 
+    elif price > 1:
+        price = round(price,2)
+    else:
+        price = round_sig(price,2)
+    return price
 
 @bot.slash_command(name="price", description="Show the current price for a coin")
 async def price(ctx, coin: str):
@@ -70,23 +93,22 @@ async def price(ctx, coin: str):
 
     if data and coin_id in data:
         price_data = data[coin_id]
-        price = price_data['usd']
-        change = round(price_data['usd_24h_change'],1)
-        cap = int(price_data['usd_market_cap'])
-        if price > 100:
-            price = int(price)
-        elif price > 1:
-            price = round(price,2)
-        else:
-            price = round_sig(price,2)
+        price = price_data['current_price']
+        change = round(price_data['price_change_percentage_24h'],1)
+        cap = int(price_data['market_cap'])
+        ath = price_data['ath']
+        ath_percentage = int(price_data['ath_change_percentage'])
+        price = format(price)
+        ath = format(ath)
+        cap = format(cap)
         if change >= 0:
             # Edit the response to send the actual content
-            await ctx.edit(content=f"The price of {coin_id} is ${price} (↗{change}%). Market Cap: ${cap:,}")
+            await ctx.edit(content=f"The price of {coin_id} is ${price} (⬈{change}%). Market Cap: ${cap}. ATH: ${ath} ({ath_percentage}%)")
         elif change > -10:
             # Edit the response to send the actual content
-            await ctx.edit(content=f"The price of {coin_id} is ${price} (↘{change}%). Market Cap: ${cap:,}")
+            await ctx.edit(content=f"The price of {coin_id} is ${price} (⬊{change}%). Market Cap: ${cap}. ATH: ${ath} ({ath_percentage}%)")
         else:
-            await ctx.edit(content=f"The price of {coin_id} is ${price} (:skull_crossbones: ↘{change}% :skull_crossbones:). Market Cap: ${cap:,}")
+            await ctx.edit(content=f"The price of {coin_id} is ${price} (:skull_crossbones: ⬊{change}% :skull_crossbones:). Market Cap: ${cap}. ATH: ${ath} ({ath_percentage}%)")
     else:
         # Edit the response to send the actual content
         await ctx.edit(content=f"Could not find a coin with the ID '{coin}'")
@@ -131,6 +153,76 @@ async def add(ctx, coins: str):
         # Edit the response to send the actual content
         await ctx.edit(content="None of the coins you provided were added. They might already be in your favorites or they are not valid.")
 
+@bot.slash_command(name="search", description="Search for coins by name and display their IDs, price, and market cap")
+async def search_coins(ctx, query: str):
+    # Defer the response
+    await ctx.defer()
+
+    # Search for coins that match the query using the CoinGecko API
+    response = requests.get(f'https://api.coingecko.com/api/v3/coins/list?search={query}')
+    matching_coins = response.json()
+
+    # If there are fewer than 50 matching coins, get their IDs, price, and market cap
+        # Get the IDs of the matching coins
+    matching_ids = [coin['id'] for coin in matching_coins[:10]]
+
+    # Fetch the prices, market cap, and 24-hour change for the matching coins
+    prices = await get_prices(matching_ids)
+
+    # Create a table
+    table = PrettyTable()
+    table.field_names = ['ID', 'Price', 'Market Cap', '24h Change']
+
+    # Add the top 5 coins to the table
+    for coin_id in matching_ids[:10]:
+        if coin_id in prices:
+            price = prices[coin_id]['usd']
+            market_cap = prices[coin_id]['usd_market_cap']
+            change = prices[coin_id]['usd_24h_change']
+            table.add_row([coin_id, price, market_cap, change])
+
+    # Send the table
+    await ctx.send(f'```\n{table}\n```')
+
+
+@bot.slash_command(name="id", description="Add a coin to your favorites by exact ID")
+async def add_coin(ctx, coin_id: str):
+    # Defer the response
+    await ctx.defer()
+
+    user_id = str(ctx.author.id)
+    favorites = {}
+
+    # Load the existing data if the file exists
+    if os.path.exists('favorites.json'):
+        with open('favorites.json', 'r') as f:
+            favorites = json.load(f)
+
+    # If the user has no favorites saved, create an empty list for them
+    if user_id not in favorites:
+        favorites[user_id] = []
+
+    # Check if the coin_id exists in coins
+    with open('coins.json', 'r') as f:
+        coins = json.load(f)
+    if any(coin['id'] == coin_id for coin in coins):
+        # Add the coin to the favorites if it's not already there
+        if coin_id not in favorites[user_id]:
+            favorites[user_id].append(coin_id)
+
+            # Save the updated favorites
+            with open('favorites.json', 'w') as f:
+                json.dump(favorites, f)
+
+            # Edit the response to send the actual content
+            await ctx.edit(content=f"Added coin to your favorites: {coin_id}")
+        else:
+            # Edit the response to send the actual content
+            await ctx.edit(content="The coin you provided is already in your favorites.")
+    else:
+        # Edit the response to send the actual content
+        await ctx.edit(content="The coin you provided is not valid.")
+
 
 @bot.slash_command(name="coins", description="Show current prices for your favorite coins")
 async def coins(ctx):
@@ -152,31 +244,35 @@ async def coins(ctx):
 
         # Create a table with headers
         table = PrettyTable()
-        table.field_names = ["Coin", "Price", "24h Change", "Market Cap"]
+        table.field_names = ["Coin", "Price", "24h Change", "Market Cap", "ATH"]
         table.align["Price"] = "r"
         table.align["Coin"] = "l"
         table.align["Market Cap"] = "r"
+        table.align["ATH"] = "r"
 
         # Create a list of coins with their data
         coins_data = []
         for coin, data in prices.items():
-            price = data['usd']
-            change = round(data['usd_24h_change'],1)
-            cap = int(data['usd_market_cap'])
+            price = data['current_price']
+            change = round(data['price_change_percentage_24h'],1)
+            cap = int(data['market_cap'])
+            ath = format(data['ath'])
+            off_ath = int(data['ath_change_percentage'])
+            ath_field = f"{ath} ({off_ath}%)"
             if price > 100:
-                price = int(price)
+                price = f'{int(price):,}' 
             elif price > 1:
                 price = round(price,2)
             else:
                 price = round_sig(price,2)
-            coins_data.append([coin, price, change, cap])
+            coins_data.append([coin, price, change, cap, ath_field])
 
         # Sort the coins by market cap in descending order
         coins_data.sort(key=lambda x: x[3], reverse=True)
 
         # Add a row for each coin
         for coin_data in coins_data:
-            table.add_row([coin_data[0], f"${coin_data[1]}", f"↗{coin_data[2]}%" if coin_data[2] >= 0 else f"↘{coin_data[2]}%", f"${coin_data[3]:,}"])
+            table.add_row([coin_data[0], f"${coin_data[1]}", f"⬈{coin_data[2]}%" if coin_data[2] >= 0 else f"⬊{coin_data[2]}%", f"${coin_data[3]:,}", coin_data[4]])
 
         # Edit the response to send the actual content
         await ctx.edit(content=f"```{table}```")
@@ -225,42 +321,47 @@ async def remove(ctx, coins: str):
         await ctx.edit(content="You don't have any favorite coins saved.")
 
 def get_bitcoin_price():
-    global count
-    global change
-    count = 0
-
-    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
-    price = response.json()['bpi']['USD']['rate_float']
-    count += 1
-
-    if count == 1:
-        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true"
-        headers = {"x-cg-demo-api-key": GECKO_API}
-        response = requests.get(url, headers=headers)
-        price = response.json()['bitcoin']['usd']
-        change = int(response.json()['bitcoin']['usd_24h_change'] * 100)/100
-    else:
-        if count > 10:
-            count = 0
-
-    return (f'{int(price):,}' , change)
-
-
+    rand = random.randint(0, 21)
+    global change_btc
+    try:
+        if rand%3 == 1 or not change_btc:
+            url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true"
+            headers = {"x-cg-demo-api-key": GECKO_API}
+            response = requests.get(url, headers=headers)
+            price_btc = response.json()['bitcoin']['usd']
+            change_btc = int(response.json()['bitcoin']['usd_24h_change'] * 100)/100
+        elif rand%3 == 0:
+            response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
+            price_btc = response.json()['bpi']['USD']['rate_float']
+        else:
+            response = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
+            price_btc = response.json()['data']['amount']
+        price_btc = f'{int(float(price_btc)):,}'
+        
+    except:
+        price_btc = .999
+        change_btc = 0
+    return (price_btc, change_btc)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     update_activity.start()  # Start the task as soon as the bot is ready
 
-@tasks.loop(minutes=1)  # Create a task that runs every minute
+@tasks.loop(minutes = 1)  # Create a task that runs every minute
 async def update_activity():
-    price, change = get_bitcoin_price()
-    if change >= 0:
-        await bot.change_presence(activity=discord.Game(name=f"${price} ↗{change}%"))
-    elif change > -10:
-        await bot.change_presence(activity=discord.Game(name=f"${price} ↘{change}%"))
-    else:
-        await bot.change_presence(activity=discord.Game(name=f"${price} (:skull_crossbones: ↘{change}% :skull_crossbones:)"))
 
-token = os.environ.get("DISCORD_BOT_SECRET")
+    price_btc, change = get_bitcoin_price()
+    if price_btc == .999:
+        return
+    if change >= 0:
+        await bot.change_presence(activity=discord.Game(name=f"${price_btc} ⬈{change}%"))
+    elif change > -10:
+        await bot.change_presence(activity=discord.Game(name=f"${price_btc} ⬊{change}%"))
+    else:
+        await bot.change_presence(activity=discord.Game(name=f"${price_btc} (:skull_crossbones: ⬊{change}% :skull_crossbones:)"))
+
+
 bot.run(token)
+
+
--- Rumbot.py
+++ Rumbot.py
@@ -2,17 +2,16 @@ import discord
 from discord.ext import tasks
 import requests
 import os
+import random
 
 
 intents = discord.Intents.default()  # Create a new Intents object with default settings
 bot = discord.Bot(intents=intents)  # Pass the Intents object to the Bot
 
-if change_eth >= 0:
-    await eth_bot.change_presence(activity=discord.Game(name=f"${price_eth} ↗{change_eth}%"))
-elif change_eth > -10:
-    await eth_bot.change_presence(activity=discord.Game(name=f"${price_eth} ↘{change_eth}%"))
-else:
-    await eth_bot.change_presence(activity=discord.Game(name=f"${price_eth} (:skull_crossbones: ↘{change_eth}% :skull_crossbones:)"))
+GECKO_API = os.environ.get("GECKO_API")
+global change_eth
+change_eth = None
+
 
 @bot.event
 async def on_ready():
@@ -20,26 +19,35 @@ async def on_ready():
     update_activity.start()  # Start the task as soon as the bot is ready
 
 def get_eth_price():
-    global count
-    count+=1
-    if count%2 == 0:
-        response = requests.get('https://api.coinbase.com/v2/prices/ETH-USD/spot')
-        price = response.json()['data']['amount']
-    else:
-        response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/ETH.json')
-        price_eth = response.json()['bpi']['USD']['rate_float']
-    if count > 20:
-        count = 0
-
-    return (f'{int(price_btc):,}' , change_btc,f'{int(price_eth):,}', change_eth)
-
+    rand = random.randint(0, 100)
+    global change_eth
+    try:
+        if rand%2 == 1 or change_eth is None:
+            url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd&include_24hr_change=true"
+            response = requests.get(url)
+            price_eth = response.json()['ethereum']['usd']
+            change_eth = int(response.json()['ethereum']['usd_24h_change'] * 100)/100
+        if rand%2 == 0:
+            response = requests.get('https://api.coinbase.com/v2/prices/ETH-USD/spot')
+            price_eth = response.json()['data']['amount']
+        price_eth = f'{int(float(price_eth)):,}'
+        
+    except:
+        price_eth = .999
+        change_eth = 0
+    return (price_eth, change_eth)
 @tasks.loop(minutes=1)  # Create a task that runs every minute
 async def update_activity():
-    price, change = get_eth_price()
-    if change >= 0:
-        await bot.change_presence(activity=discord.Game(name=f"${price} ↗{change}%"))
-    elif change > -10:
-        await bot.change_presence(activity=discord.Game(name=f"${price} ↘{change}%"))
+
+    price_eth, change_eth = get_eth_price()
+    if price_eth == .999:
+        return
+    if change_eth >= 0:
+        await bot.change_presence(activity=discord.Game(name=f"${price_eth} ⬈{change_eth}%"))
+    elif change_eth > -10:
+        await bot.change_presence(activity=discord.Game(name=f"${price_eth} ⬊{change_eth}%"))
+    else:
+        await bot.change_presence(activity=discord.Game(name=f"${price_eth} (:skull_crossbones: ⬊{change_eth}% :skull_crossbones:)"))
 
 token = os.environ.get("ETH_BOT_SECRET")
 bot.run(token)
