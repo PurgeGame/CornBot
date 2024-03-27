aaa394208981295c8b6adbb32355fefbc7d409a9
Purge Game <purgegamenft@gmail.com>
2024-03-27 03:32:34 -0500

    cleanup, remove 1m mc filter

--- CornBot.py
+++ CornBot.py
@@ -8,47 +8,47 @@ import aiohttp
 import random
 from dotenv import load_dotenv
 from typing import Optional
+import asyncio
 
 
 load_dotenv()
 
-
-GECKO_API = os.environ.get("GECKO_API")
-
-intents = discord.Intents.default()  # Create a new Intents object with default settings
-bot = discord.Bot(intents=intents)  # Pass the Intents object to the Bot
-
 token = os.environ.get("DISCORD_BOT_SECRET")
+GECKO_API = os.environ.get("GECKO_API")
+intents = discord.Intents.default() 
+bot = discord.Bot(intents=intents)  
 global change_btc
 change_btc = None
 
-
+async def fetch_data_from_api(url):
+    headers = {"x-cg-demo-api-key": GECKO_API}
+    # Send a request to the CoinGecko API
+    async with aiohttp.ClientSession() as session:
+        for i in range(2):  # Try twice
+            if i == 0:
+                async with session.get(url) as response:  # First try without headers
+                    if response.status == 200:
+                        data = await response.json()
+                        return data
+                    else:
+                        await asyncio.sleep(5)  # Wait for 5 seconds before the next try
+            else:
+                async with session.get(url, headers=headers) as response:  # Second try with headers
+                    if response.status == 200:
+                        data = await response.json()
+                        return data
+    return {}
 
 async def get_prices(coins):
     # Convert the list of coins to a comma-separated string
     coins_str = ', '.join(coin for coin in coins if coin is not None)
-
-    # Define the URL and headers
     url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coins_str}"
-    headers = {"x-cg-demo-api-key": GECKO_API}
+    data = await fetch_data_from_api(url)
+    # Convert the list to a dictionary
+    prices = {coin['id']: coin for coin in data}
+    # Return the prices
+    return prices
 
-    # Send a request to the CoinGecko API
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as response:
-            data = await response.json()
-
-    # Check if the request was successful
-    if response.status == 200:
-        # Convert the list to a dictionary
-        prices = {coin['id']: coin for coin in data}
-
-        # Return the prices
-        return prices
-    else:
-        # If the request was not successful, return an empty dictionary
-        return {}
-    
-    
 async def check_coin(coin):
     coin = coin.lower()
     matching_ids = []
@@ -68,56 +68,27 @@ async def check_coin(coin):
 async def get_coin_with_lowest_market_cap_rank(coin_ids):
     # Convert the list of coin IDs to a comma-separated string
     coin_ids_str = ', '.join(coin_ids)
-
-    # Define the URL
     url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coin_ids_str}"
+    data = await fetch_data_from_api(url)
+    # Find the coin with the lowest market cap rank
+    lowest_rank_id = min(data, key=lambda coin: coin['market_cap_rank'] if coin['market_cap_rank'] is not None else float('inf'))['id']
+    return lowest_rank_id
 
-    # Send a request to the CoinGecko API
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-
-    # Check if the request was successful
-    if response.status == 200:
-        # Find the coin with the lowest market cap rank
-        lowest_rank_id = min(data, key=lambda coin: coin['market_cap_rank'] if coin['market_cap_rank'] is not None else float('inf'))['id']
-
-        # Return the ID of the coin with the lowest market cap rank
-        return lowest_rank_id
-    else:
-        # If the request was not successful, return None
-        return None
 
 @bot.slash_command(name="price", description="Show the current price for a coin")
 async def price(ctx, coins: str):
-    # Defer the response
     await ctx.defer()
-
     # Split the coins parameter by commas to get a list of coins
     coins = [coin.strip() for coin in coins.split(',')]
-    
-    # Create a dictionary to store the coin data
     coins_data = {}
-
-    # Loop over the list of coins
     for coin in coins:
-        # Check the coin
         coin_id = await check_coin(coin)
         if not coin_id:
-            # If the coin is not valid, continue to the next coin
             continue
-
-        # Fetch the current price for the coin
         data = await get_prices([coin_id])
-
         if data and coin_id in data:
-            # Add the coin data to the dictionary
             coins_data[coin_id] = data[coin_id]
-
-    # Use the display_coins function to display the coin data
     await display_coins(ctx, coins_data)
-
-
 
 def is_number(n):
     try:
@@ -132,7 +103,6 @@ def round_sig(num, sig_figs):
     else:
         return 0  # Can't take the log of 0
 
-
 def format_number(num):
     if num is None or not is_number(num):
         return 0
@@ -145,7 +115,7 @@ def format_number(num):
         elif num == 0:
             return 0
         elif num < 0.01:
-            return f'{num:.2e}'  # use scientific notation for very small numbers
+            return f'{num:.2e}'
         elif num > 1:
             return round(num, 2)
         elif num > .01:
@@ -153,47 +123,26 @@ def format_number(num):
         else:
             return round_sig(num, 2)
         
-
 async def display_coins(ctx, coins_data, display_id=False, list_name=None):
     # Create a table
     table = PrettyTable()
     table.field_names = ['ID' if display_id else 'Name', 'Price', 'Δ 24h', 'Market Cap', 'Rank', 'ATH', 'Δ ATH']
-    table.align = 'r'  # right-align data
-    table.align['ID' if display_id else 'Name'] = 'l'  # left-align IDs
+    table.align = 'r' 
+    table.align['ID' if display_id else 'Name'] = 'l' 
+    # Filter coins with market cap >= 1 million
+    filtered_coins = {coin_id: coin_data for coin_id, coin_data in coins_data.items()}# if coin_data['market_cap'] and coin_data['market_cap'] >= 1000000}
 
-    # Add a counter for the number of coins added to the table
-    num_coins = 0
-
-    for coin_id, coin_data in coins_data.items():
-        prices = coin_data
-        market_cap = prices['market_cap']
-        if market_cap is not None:
-            if market_cap < 1000000 and market_cap !=0:
-                continue
-        else:
-            continue
-        market_cap = format_number(market_cap)  
-
-        price = format_number(prices['current_price']) if prices['current_price'] is not None else 'N/A'
-        change = prices['price_change_percentage_24h']
-        if change is not None:
-            symbol = '+' if change >= 0 else '-'
-            change = f"{symbol}{abs(change):.1f}"
-        else:
-            change = 'N/A'
-        ath = format_number(prices['ath']) if prices['ath'] is not None else 'N/A'
-        ath_change = prices['ath_change_percentage']
-        ath_change = 'N/A' if ath_change is None else ath_change
-
-        mc_rank = prices['market_cap_rank'] if prices['market_cap_rank'] is not None else 'N/A'
-
+    for coin_id, prices in filtered_coins.items():
+        market_cap = format_number(prices['market_cap'])  
+        price = format_number(prices['current_price']) if prices['current_price'] else 'N/A'
+        change = f"{'+-'[prices['price_change_percentage_24h'] < 0]}{abs(prices['price_change_percentage_24h']):.1f}" if prices['price_change_percentage_24h'] else 'N/A'
+        ath = format_number(prices['ath']) if prices['ath'] else 'N/A'
+        ath_change = prices['ath_change_percentage'] if prices['ath_change_percentage'] else 'N/A'
+        mc_rank = prices['market_cap_rank'] if prices['market_cap_rank'] else 'N/A'
         table.add_row([coin_id, price, f'{change}%', f'{market_cap}', mc_rank, ath, f"{ath_change:02.0f}%"])
-
-        # Increment the counter
-        num_coins += 1
-
+        
     # Check if any coins were added to the table
-    if num_coins == 0:
+    if not filtered_coins:
         await ctx.edit(content='No coins with a market cap of $1 million or more were found.')
     else:
         # Send the table
@@ -204,22 +153,14 @@ async def display_coins(ctx, coins_data, display_id=False, list_name=None):
 
 @bot.slash_command(name="coins", description="Show current prices for your favorite coins")
 async def coins(ctx, list_name: Optional[str] = None):
-    # Defer the response
     await ctx.defer()
-
     user_id = str(ctx.author.id)
     favorites = {}
-
-    # Load the existing data if the file exists
     if os.path.exists('favorites.json'):
         with open('favorites.json', 'r') as f:
             favorites = json.load(f)
-
-    # Check if a list name was provided
     if list_name:
-        # Check if the list name exists in the favorites
         if list_name in favorites:
-            # Use the coins from the provided list
             coins = favorites[list_name]
         else:
             await ctx.edit(content=f"The list '{list_name}' does not exist.")
@@ -228,14 +169,9 @@ async def coins(ctx, list_name: Optional[str] = None):
         # Use the user's favorite coins if no list was provided
         coins = favorites[user_id]
     else:
-        # Edit the response to send the actual content
         await ctx.edit(content="You don't have any favorite coins saved.")
         return
-
-    # Fetch the current prices for all coins
     prices = await get_prices(coins)
-
-    # Display the coins
     await display_coins(ctx, prices, list_name=list_name)
 
 @bot.slash_command(name="search", description="Search for coins by name and display their IDs, price, and market cap")
@@ -244,17 +180,10 @@ async def search_coins(ctx, query: str, limit: Optional[int] = 10):
         limit = 50
     # Defer the response
     await ctx.defer()
-
-    # Create an aiohttp.ClientSession
-    async with aiohttp.ClientSession() as session:
-        # Search for coins that match the query using the CoinGecko API
-        async with session.get(f'https://api.coingecko.com/api/v3/search?query={query}') as response:
-            matching_coins = await response.json()
-
+    url = f'https://api.coingecko.com/api/v3/search?query={query}'
+    matching_coins = await fetch_data_from_api(url)
     # Get the IDs of the top 10 matching coins
     matching_ids = [coin['id'] for coin in matching_coins['coins'][:limit]]
-   
-
     # Fetch the prices, market cap, 24-hour change, ath, and ath_change_percentage for the matching coins
     prices = await get_prices(matching_ids)
 
@@ -268,11 +197,9 @@ async def get_bitcoin_price():
         async with aiohttp.ClientSession() as session:
             if rand%3 == 1 or not change_btc:
                 url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true"
-                headers = {"x-cg-demo-api-key": GECKO_API}
-                async with session.get(url, headers=headers) as response:
-                    data = await response.json()
-                    price_btc = data['bitcoin']['usd']
-                    change_btc = int(data['bitcoin']['usd_24h_change'] * 100)/100
+                data = await fetch_data_from_api(url)
+                price_btc = data['bitcoin']['usd']
+                change_btc = int(data['bitcoin']['usd_24h_change'] * 100)/100
             elif rand%3 == 0:
                 async with session.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json') as response:
                     data = await response.json()
