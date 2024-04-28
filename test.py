db20af1439d2798cc40b763e5877f2d96e27a4fe
Purge Game <purgegamenft@gmail.com>
2024-04-28 00:19:21 -0500

    cancel that

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-import aiohttp
+
 import os
 from dotenv import load_dotenv
-import asyncio
+
 load_dotenv()
 MAGIC_EDEN_API = os.getenv("MAGIC_EDEN_API")
 import requests
 
-url = "https://api-mainnet.magiceden.dev/v2/ord/btc/runes/collection_stats/search?window=1d&limit=2&sort=floorPrice&direction=desc"
+url = "https://api-mainnet.magiceden.dev/v2/ord/btc/runes/collection_stats/search?window=1d&sort=floorPrice&direction=desc"
+
 
 
 headers = {"Authorization": f"Bearer {MAGIC_EDEN_API}"}
