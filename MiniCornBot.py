9c87ea79f36fc261cfd41f13ab06b2393aadd253
Purge Game <purgegamenft@gmail.com>
2024-12-17 22:47:43 -0600

    add minicornbot

---
+++ MiniCornBot.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import tasks
+import aiohttp
+import os
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+intents = discord.Intents.default()  # Create a new Intents object with default settings
+bot = discord.Bot(intents=intents)  # Pass the Intents object to the Bot
+
+GECKO_API = os.environ.get("GECKO_API")
+global change_btc
+change_btc = None
+global ratio
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    update_activity.start()  # Start the task as soon as the bot is ready
+
+async def get_btc_price():
+    rand = random.randint(0, 100)
+    global change_btc
+    global ratio
+    try:
+        async with aiohttp.ClientSession() as session:
+            if rand%2 == 1 or change_btc is None:
+                url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true"
+                async with session.get(url) as response:
+                    data = await response.json()
+                price_btc = data['bitcoin']['usd']
+                change_btc = int(data['bitcoin']['usd_24h_change'] * 100)/100
+            if rand%2 == 0:
+                async with session.get('https://api.coinbase.com/v2/prices/BTC-USD/spot') as response:
+                    data = await response.json()
+                price_btc = data['data']['amount']
+        price_btc = f'{int(float(price_btc)):,}'
+        
+    except:
+        price_btc = .999
+        change_btc = 0
+        ratio = 0
+    return (price_btc, change_btc)
+
+@tasks.loop(minutes=1)  # Create a task that runs every minute
+async def update_activity():
+
+    price_btc, change_btc = await get_btc_price()
+    if price_btc == .999:
+        return
+    if change_btc >= 0:
+        await bot.change_presence(activity=discord.Game(name=f"${price_btc} ⬈{abs(change_btc):.1f}%"))
+    else:
+        await bot.change_presence(activity=discord.Game(name=f"${price_btc} ⬊{abs(change_btc):.1f}%"))
+
+token = os.environ.get("DISCORD_BOT_SECRET")
+bot.run(token)
